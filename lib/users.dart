5048d899ed235167beb50beaaa4a0867f267539f
Jeremiah Larry King Ungsod <67980301+Jeremiah019@users.noreply.github.com>
2022-10-29 22:14:55 +0800

    Create class user

---
+++ lib/users.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class User {
+  String? name;
+  String? relationship;
+  String? occupation;
+  String? birthday;
+  int? age;
+  String icon;
+
+  User({this.name, this.relationship, this.occupation, this.age, this.birthday, required this.icon,});
+}
